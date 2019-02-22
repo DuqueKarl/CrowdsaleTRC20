6e41655baecd6e9ed5a532a241713476bba5a2eb
LAPTOP-9A4TGPS4\ppin <duquekarl@gmail.com>
2019-02-22 01:50:55 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,27 +1,20 @@
 const MyToken = artifacts.require("./MyToken.sol");
 const MyTokenCrowdsale = artifacts.require("./MyTokenCrowdsale.sol");
 
-module.exports = function(deployer, network, accounts) {
+const web3 = require("web3-utils");
+
+module.exports = (deployer, network, [owner]) => {
+
   const _name = "My Token TRC20";
   const _symbol = "MYT";
   const _decimals = 18;
   const _initialSupply = 16000000;
   const _rate = 500;
-  const _wallet = accounts[0];
-  
-  let token, crowdsale;
+  const _crowdsaleTokens = web3.toWei("8000000", "ether");
+  	
   return deployer
-    .then(() => {
-      return deployer.deploy(MyToken, _name, _symbol, _decimals, _initialSupply);
-    })
-    .then((instance) => {
-	  token = instance;
-      return deployer.deploy(MyTokenCrowdsale, _rate, _wallet, MyToken.address, _wallet);
-    })
-	.then((instance) => {
-	  crowdsale = instance;
-	  token.transferOwnership(crowdsale.address);	  
-      token.approve(crowdsale.address, _initialSupply * 10 ** 18 );
-	});		
-
+	.then(() => deployer.deploy(MyToken, _name, _symbol, _decimals, _initialSupply))
+	.then(() => deployer.deploy(MyTokenCrowdsale, _rate, owner, MyToken.address, owner))
+	.then(() => MyToken.deployed())
+	.then(token => token.approve(MyTokenCrowdsale.address, _crowdsaleTokens ));
 };
